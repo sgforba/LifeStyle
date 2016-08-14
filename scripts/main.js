85a71766ed5f5f9b4e4307db02a7f6272c062e22
sgforba <sgforba@gmail.com>
2016-08-14 12:39:35 -0400

    Added gallery images and scrolling ability

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
-	
+
+	$('.insta-gallery-left-arrow').on('click', function(){
+		var firstInsta = $('.insta-slide:first');
+		// var pos=$(firstInsta).offset().left;
+		// $(firstInsta).offset({ left : pos-293});
+		$('.insta-slider').append(firstInsta);
+	});
+
+	$('.insta-gallery-right-arrow').on('click', function(){
+		var lastInsta = $('.insta-slide:last');
+		// var pos=$(lastInsta).offset().right;
+		// $(lastInsta).offset({ right : pos+293});
+		$('.insta-slider').prepend(lastInsta);
+		
+	});
+
+	$('.image-gallery-overlay-left').on('click', function(){
+		var firstItem = $('.slider-item:first');
+		$('.slider').append(firstItem);
+	});
+
+	$('.image-gallery-overlay-right').on('click', function(){
+		var lastItem = $('.slider-item:last');
+		$('.slider').prepend(lastItem);
+		
+	});	
+
 });
